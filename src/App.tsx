1777e70463edbf689d4c3321a7b8101dac2871fd
terjetop <terjetop@uia.no>
2023-11-10 16:45:37 +0100

    Added setup component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Setup } from './components/TypeScriptReactSetup';
 
 const App = () =>{
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header >
+        <div style={{fontWeight: 'bold', fontSize: 25, margin: 20}}>Learn-code</div>
       </header>
+      <body>
+        <Setup/>
+      </body>
     </div>
   );
 }
---
+++ src/components/TypeScriptReactSetup.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const Setup = () => {
+    return (
+        <div>
+            <div style={{fontSize: 20, }}>
+                To set up an environment for <em>React</em> and <em>Typescript</em>, 
+                I followed <a href="https://create-react-app.dev/docs/adding-typescript/">this setup</a>.
+            </div>
+            <div>
+                In this way you get a setup ready to work on. It also includes a test library that I will not cover in debts now.
+            </div>
+            <div>You will probably get a warning like this:</div>
+            <div></div>
+        </div>
+    )
+}
