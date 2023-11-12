a5647b5cec5938a0cac2fda6ed417891be8ae4d4
terjetop <terjetop@uia.no>
2023-11-12 09:11:41 +0100

    create-react-app is dead. Hence I will update or create a new project with Next.js

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,9 @@ const App = () =>{
     <div className="App">
       <header >
         <h1>Learn-code</h1>
+        <div style={{color: 'rgb(250, 30, 30)', fontSize: 22}}>
+          Since I have found out that create-react-app is "dead", I will update the project to Next.js
+          </div>
       </header>
       <Setup/>
     </div>
